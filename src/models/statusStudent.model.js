6bc8be2c09aaba275f9df645853ebcea9440a636
unknown <duclnph24870@fpt.edu.vn>
2023-04-06 13:29:27 +0700

    #184 - feat/createStatusStudent - @duclnph24870 - feat: CRUD status tudent features

---
+++ src/models/statusStudent.model.js
@@ -0,0 +1,28 @@
+import { model, Schema } from 'mongoose';
+const { ObjectId } = Schema;
+
+const StatusSchema = Schema(
+	{
+		value: {
+			type: Number,
+			unique: true,
+			require: true,
+		},
+		title: {
+			type: String,
+			require: true,
+		},
+		campus: {
+			type: ObjectId,
+			require: true,
+			ref: 'Cumpus',
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const StatusStudentModel = model('statusStudent', StatusSchema);
+
+export default StatusStudentModel;
