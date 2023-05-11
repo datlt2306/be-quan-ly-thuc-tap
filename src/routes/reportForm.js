0a87a72906cb1a18e04e4c4e47d0df841165fbe8
Maladroy <hoamgchilam@pm.me>
2023-05-11 16:53:58 +0700

    hotfix: settime error

--- src/routes/reportForm.js
+++ src/routes/reportForm.js
@@ -1,24 +1,21 @@
-const { report, form } = require("../controllers/reportFromController");
-const {
-  isAuthenticateUser,
-  authorizeRoles,
-} = require("../middlewares/CheckAuth");
-const { checkRequestTime } = require("../middlewares/CheckTimeRequest");
-const router = require("express").Router();
+const { report, form } = require('../controllers/reportFromController');
+const { isAuthenticateUser, authorizeRoles } = require('../middlewares/CheckAuth');
+const { checkRequestTime } = require('../middlewares/CheckTimeRequest');
+const router = require('express').Router();
 
 router.patch(
-  "/report",
-  isAuthenticateUser,
-  // authorizeRoles([1, 2]),
-  checkRequestTime,
-  report
+	'/report',
+	isAuthenticateUser,
+	// authorizeRoles([1, 2]),
+	checkRequestTime,
+	report
 );
 router.patch(
-  "/form",
-  isAuthenticateUser,
-  // authorizeRoles([1, 2]),
-  checkRequestTime,
-  form
+	'/form',
+	isAuthenticateUser,
+	// authorizeRoles([1, 2]),
+	checkRequestTime,
+	form
 );
 
 module.exports = router;
