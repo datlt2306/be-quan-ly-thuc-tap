52e0134c805ebd38ef3c6ff93a45aab4f0fa1799
Maladroy <hoamgchilam@pm.me>
2023-05-11 17:17:18 +0700

    fix: report & form endpoint now begin with /upload

--- src/routes/reportForm.js
+++ src/routes/reportForm.js
@@ -1,24 +1,21 @@
-const { report, form } = require("../controllers/reportFromController");
-const {
-  isAuthenticateUser,
-  authorizeRoles,
-} = require("../middlewares/CheckAuth");
-const { checkRequestTime } = require("../middlewares/CheckTimeRequest");
-const router = require("express").Router();
+const { report, form } = require('../controllers/reportFromController');
+const { isAuthenticateUser, authorizeRoles } = require('../middlewares/CheckAuth');
+const { checkRequestTime } = require('../middlewares/CheckTimeRequest');
+const router = require('express').Router();
 
 router.patch(
-  "/report",
-  isAuthenticateUser,
-  // authorizeRoles([1, 2]),
-  checkRequestTime,
-  report
+	'upload/report',
+	isAuthenticateUser,
+	// authorizeRoles([1, 2]),
+	checkRequestTime,
+	report
 );
 router.patch(
-  "/form",
-  isAuthenticateUser,
-  // authorizeRoles([1, 2]),
-  checkRequestTime,
-  form
+	'upload/form',
+	isAuthenticateUser,
+	// authorizeRoles([1, 2]),
+	checkRequestTime,
+	form
 );
 
 module.exports = router;
