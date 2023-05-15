7b65bd89e6fb4f7ba015051e41bd2ecea18bfd5e
Maladroy <hoamgchilam@pm.me>
2023-05-15 15:32:42 +0700

    fix: populate campus_id in manager

--- src/models/manager.js
+++ src/models/manager.js
@@ -1,26 +1,30 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const paginate = require('mongoose-paginate-v2');
+
 const managerSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  campus_id: {
-    type: mongoose.Schema.ObjectId,
-    ref: "Cumpus",
-    require: true,
-  },
-  role: {
-    type: Number,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	campus_id: {
+		type: mongoose.Schema.ObjectId,
+		ref: 'Cumpus',
+		require: true,
+	},
+	role: {
+		type: Number,
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
 });
 
-module.exports = mongoose.model("Manager", managerSchema);
+managerSchema.plugin(paginate);
+
+module.exports = mongoose.model('Manager', managerSchema);
