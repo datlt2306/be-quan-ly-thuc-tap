988b1d86dbba3ed9ec57a3c74e5cc0ee893f3492
Maladroy <hoamgchilam@pm.me>
2023-05-16 20:58:26 +0700

    feat: add upload API that store and share file URL

---
+++ src/api/routes/googleDrive.route.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { uploadFile } from '../controllers/googleDrive.controller';
+import { authorizeRoles, isAuthenticateUser } from '../middlewares/CheckAuth';
+import { role } from '../../utils/role';
+import multer from 'multer';
+
+const upload = multer();
+const router = express.Router();
+
+router.post(
+	'/drive/upload',
+	upload.any(),
+	isAuthenticateUser,
+	uploadFile
+);
+
+module.exports = router;
+    
