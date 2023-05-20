bac9837ea5e722d9d286f5164a8f6be52036005c
quanghiep03198 <quanghiep03198@gmail.com>
2023-05-20 19:15:53 +0700

    Fix: use routers

--- index.js
+++ index.js
@@ -1,49 +1,37 @@
 import compression from 'compression';
 import cors from 'cors';
-import express from 'express';
-import { readdir, readdirSync } from 'fs';
-import morgan from 'morgan';
-import swaggerOptions from './src/config/swagger.config';
-import path from 'path';
-import swaggerUI from 'swagger-ui-express';
 import 'dotenv/config';
+import express from 'express';
+import { readdirSync } from 'fs';
+import morgan from 'morgan';
+import swaggerUI from 'swagger-ui-express';
+import swaggerOptions from './src/config/swagger.config';
 import connectMongo from './src/database/mongo.db';
+
 const app = express();
 
+// Route
+const ROUTES_DIR = './src/api/routes/';
+const appRouteModules = readdirSync(ROUTES_DIR).map((route) => import(ROUTES_DIR + route));
+Promise.all(appRouteModules)
+	.then((routes) => routes.forEach((route) => app.use('/api', route.default)))
+	.catch((error) => console.log(error.message));
 
 // middleware
+app.use(express.json());
 app.use(morgan('tiny'));
 app.use(express.json({ limit: '50mb' }));
 app.use(cors());
 app.use(compression({ level: 6, threshold: 1 * 1024 })); // compress data if payload is too large
-// Route
-const routedDir = path.resolve(path.join(__dirname, "./src/api/routes/")) 
-readdir(routedDir, (err, files) => {
-	if (err) {
-		console.error('Error reading directory:', err);
-		return;
-	}
-
-	// Import each file
-	files.forEach((file) => {
-		// Check if the file is a JavaScript file
-		if (file.endsWith('.js')) {
-			// Resolve the full file path
-			const filePath = path.join(routedDir, file);
-			// Import the file
-			import(filePath);
-
-		}
-	});
-});
-app.use(express.json());
 
 // swagger
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerOptions));
-app.use('*', (req, res) => {
-	res.redirect('/api-docs');
-});
 
-const port = process.env.PORT || 9998;
-app.listen(port, () => console.log('server is listening port: ', port));
+// Run server
+const PORT = process.env.PORT || 9998;
+app.listen(PORT, () => console.log('[Success] Server is listening on port: ', PORT));
+
+// Connect database
 connectMongo();
+
+export default app;
