80ab198c980aae22c706f90ed2a89bbc1e900e7b
quanghiep03198 <quanghiep03198@gmail.com>
2023-05-21 01:32:45 +0700

    Fix: CORS error

--- src/index.js
+++ src/index.js
@@ -11,15 +11,25 @@ import connectMongo from './database/mongo.db';
 const app = express();
 
 // Route
-app.use('/api',rootRouter)
+app.use('/api', rootRouter);
 
 // middleware
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(express.json({ limit: '50mb' }));
-app.use(cors());
+app.use(
+	cors({
+		origin: '*',
+		methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+	})
+);
 app.use(compression({ level: 6, threshold: 1024 })); // compress data if payload is too large
+app.use((_req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', '*');
 
+	next();
+});
 // swagger
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerOptions));
 
@@ -27,10 +37,12 @@ app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerOptions));
 const PORT = process.env.PORT || 9998;
 app.listen(PORT, () => console.log('[Success] Server is listening on port: ', PORT));
 
-app.get('/', (req, res) => res.json({
-   message: 'Connected to server !',
-   status: 200
-}))
+app.get('/', (req, res) =>
+	res.json({
+		message: 'Connected to server !',
+		status: 200,
+	})
+);
 
 // Connect database
 connectMongo();
