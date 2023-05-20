471f3925e68c5d4491d8b751609dfed6839b1211
quanghiep03198 <quanghiep03198@gmail.com>
2023-05-21 02:53:42 +0700

    Fix: CORS error on vercel

--- src/index.js
+++ src/index.js
@@ -17,12 +17,7 @@ app.use('/api', rootRouter);
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(express.json({ limit: '50mb' }));
-app.use(
-	cors({
-		origin: [process.env.FE_DEV_DOMAIN, process.env.FE_PRODUCTION_DOMAIN],
-		methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
-	})
-);
+app.use(cors());
 app.use(compression({ level: 6, threshold: 1024 })); // compress data if payload is too large
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
