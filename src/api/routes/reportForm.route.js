2ef9b1ab7df0b743a23a3c8eef5fe7da501bfaea
Maladroy <hoamgchilam@pm.me>
2023-05-22 01:34:56 +0700

    fix: reportForm endpoints and add upload API

--- src/api/routes/reportForm.route.js
+++ src/api/routes/reportForm.route.js
@@ -2,12 +2,15 @@ const { report, form } = require('../controllers/reportForm.controller');
 const { isAuthenticateUser, authorizeRoles } = require('../middlewares/CheckAuth');
 const { checkRequestTime } = require('../middlewares/CheckTimeRequest');
 const router = require('express').Router();
+const multer = require('multer');
+const upload = multer();
 
 router.patch(
 	'/report',
 	isAuthenticateUser,
 	// authorizeRoles([1, 2]),
 	checkRequestTime,
+	upload.any(),
 	report
 );
 router.patch(
@@ -15,6 +18,7 @@ router.patch(
 	isAuthenticateUser,
 	// authorizeRoles([1, 2]),
 	checkRequestTime,
+	upload.any(),
 	form
 );
 
