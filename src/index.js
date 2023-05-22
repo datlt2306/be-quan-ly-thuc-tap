4fdb21a69a151aa15612d5a300db61f226300086
quanghiep03198 <quanghiep03198@gmail.com>
2023-05-22 11:14:11 +0700

    Fix: use routes

--- src/index.js
+++ src/index.js
@@ -9,23 +9,25 @@ import swaggerOptions from './config/swagger.config';
 import connectMongo from './database/mongo.db';
 import { jobSchedule } from './cronjobs';
 import { readdirSync } from 'fs';
+import path from 'path';
 
 const app = express();
 
-// Route
-
-// middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(express.json({ limit: '50mb' }));
 app.use(compression({ level: 6, threshold: 1024 })); // compress data if payload is too large
 
-// swagger
-// app.use('/api', rootRouter);
+// Swagger
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerOptions));
-readdirSync('./api/routes').forEach((file) =>
-	import(`./api/routes/${file}`)
+
+// Routes
+// app.use('/api', rootRouter);
+const ROUTES_DIR = path.resolve(path.join(__dirname, './api/routes'))
+readdirSync(ROUTES_DIR).forEach((file) =>
+	import(path.resolve(path.join(ROUTES_DIR,file)))
 		.then((module) => app.use('/api', module.default))
 		.catch((error) => console.log(error.message))
 );
@@ -37,6 +39,8 @@ app.listen(PORT, () => {
 	jobSchedule.init();
 });
 
+
+// Default route
 app.get('/', (req, res) =>
 	res.json({
 		message: 'Connected to server !',
