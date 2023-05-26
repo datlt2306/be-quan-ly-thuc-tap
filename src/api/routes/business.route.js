bbdbe2794693190e2ce0d556fd48d2a3fdf81659
Maladroy <hoamgchilam@pm.me>
2023-05-26 12:41:10 +0700

    fix: update & create logic, HTTP Code, and validation

--- src/api/routes/business.route.js
+++ src/api/routes/business.route.js
@@ -3,6 +3,7 @@ import {
 	createbusiness,
 	getBusiness,
 	insertBusiness,
+	insertBusinessList,
 	listBusiness,
 	removeBusiness,
 	updateBusiness,
@@ -12,13 +13,16 @@ import { authorizeRoles, isAuthenticateUser } from '../middlewares/CheckAuth';
 import { role } from '../../utils/role';
 const router = express.Router();
 
+router.put('/business', isAuthenticateUser, insertBusinessList);
+
+//! DEPRECATED POST /business, POST /business/new
 router.post('/business', isAuthenticateUser, authorizeRoles([role.manager]), insertBusiness);
 router.post('/business/new', isAuthenticateUser, authorizeRoles([role.manager]), createbusiness);
-router.get('/business', isAuthenticateUser, listBusiness);
-router.delete('/business/:id', isAuthenticateUser, authorizeRoles([role.manager]), removeBusiness);
-router.get('/business/:id', isAuthenticateUser, getBusiness);
 router.patch('/business/:id', isAuthenticateUser, authorizeRoles([role.manager]), updateBusiness);
 router.patch('/business', isAuthenticateUser, authorizeRoles([role.manager]), updateMany);
 
-export default router;
+router.get('/business', isAuthenticateUser, listBusiness);
+router.get('/business/:id', isAuthenticateUser, getBusiness);
+router.delete('/business/:id', isAuthenticateUser, authorizeRoles([role.manager]), removeBusiness);
 
+export default router;
