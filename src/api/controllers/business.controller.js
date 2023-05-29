bbe7d168cfd0b9ae9d8ca1bb9ffd7e7fb76f234f
Maladroy <hoamgchilam@pm.me>
2023-05-29 12:03:56 +0700

    fix: change HTTP response, add optional query to GET

--- src/api/controllers/business.controller.js
+++ src/api/controllers/business.controller.js
@@ -5,6 +5,7 @@ import createHttpError from 'http-errors';
 import { businessListValidation, businessValidation } from '../validation/business.validation';
 import { upsertBusinessList } from '../services/business.service';
 import { HttpException } from '../../utils/httpException';
+import { isValidObjectId } from 'mongoose';
 
 //* New Route
 // [PUT] /api/business
@@ -18,7 +19,7 @@ export const insertBusinessList = async (req, res) => {
 	try {
 		const { _id: semester_id } = await getCurrentSemester(campus_id);
 
-		if (!body.length) throw createHttpError(403, 'Dữ liệu phải là một mảng array');
+		if (!body.length) throw createHttpError(400, 'Dữ liệu phải là một mảng array');
 		if (!campus_id) throw createHttpError(404, 'Không tìm thấy cơ sở hiện tại');
 		if (!semester_id && !customSemester) throw createHttpError(404, 'Không tìm thấy kỳ học hiện tại');
 
@@ -41,7 +42,7 @@ export const insertBusiness = async (req, res) => {
 
 		const { error } = businessListValidation(data);
 
-		if (error) throw createHttpError(400, error.message);
+		if (error) throw createHttpError(400, 'Dữ liệu không hợp lệ!');
 
 		// lấy ra kỳ học hiện tại
 		const semester = await getCurrentSemester(campus);
@@ -87,31 +88,34 @@ export const insertBusiness = async (req, res) => {
 
 // [GET] /api/business
 export const listBusiness = async (req, res) => {
-	const { page = 1, limit = 10, semester_id: custom_semester_id } = req.query;
+	const { page = 1, limit = 30, semester_id: customSemesterID, ...optionalQuery } = req.query;
 	const campus_id = req.campusManager || req.campusStudent;
-	const semester_id = custom_semester_id || getDefaultSemester(campus_id);
+	const semester_id = customSemesterID || getDefaultSemester(campus_id);
 
 	try {
 		// lấy ra học kỳ hiện tại
-		if (!campus_id) throw createHttpError.BadRequest('Không tìm thấy cơ sở');
-		if (!semester_id) throw createHttpError.BadRequest('Không tìm thấy kỳ học');
+		if (!campus_id) throw createHttpError(400, 'Không tìm thấy cơ sở');
+		if (!semester_id) throw createHttpError(400, 'Không tìm thấy kỳ học');
 
-		const result = await BusinessModel.paginate(
-			{
-				campus_id,
-				semester_id
-			},
-			{
-				page: Number(page),
-				limit: Number(limit),
-				sort: { created_at: 'desc' },
-				populate: ['major'],
-				customLabels: {
-					totalDocs: 'total',
-					docs: 'list'
-				}
+		let options = {
+			page: Number(page),
+			limit: Number(limit),
+			sort: { created_at: 'desc' },
+			populate: ['major'],
+			customLabels: {
+				totalDocs: 'total',
+				docs: 'list'
 			}
-		);
+		};
+
+		let filters = {
+			campus_id,
+			semester_id
+		};
+
+		if (optionalQuery) filters = { ...filters, ...optionalQuery };
+
+		const result = await BusinessModel.paginate(filters, options);
 
 		return res.status(200).json(result);
 	} catch (error) {
@@ -122,44 +126,27 @@ export const listBusiness = async (req, res) => {
 
 // [DELETE] /api/business/:id
 export const removeBusiness = async (req, res) => {
-	const campus = req.campusManager || req.campusStudent;
-	const id = req.params.id;
 	try {
-		// lấy ra học kỳ hiện tại
-		const semester = await getCurrentSemester(campus);
-		const business = await BusinessModel.findOne({
-			_id: id,
-			campus_id: campus,
-			semester_id: semester._id
-		});
+		const id = req.params.id;
 
-		if (!business) {
-			throw createHttpError(404, 'Doanh nghiệp này không tồn tại!');
-		}
+		if (!isValidObjectId(id)) throw createHttpError(400, 'ID không hợp lệ');
 
-		// kiểm tra có học sinh nào còn đăng ký doanh nghiệp không
-		const isStudentOfBusiness = await Student.findOne({
-			business: id,
-			campus_id: business.campus_id,
-			semester_id: business.semester_id
-		});
+		const hasStudentApplied = await Student.findOne({ business: id }); // Kiểm tra có học sinh nào còn đăng ký doanh nghiệp không
 
-		if (isStudentOfBusiness) {
-			throw createHttpError(403, 'Doanh nghiệp đang được sinh viên đăng ký không thể xóa.');
-		}
+		if (hasStudentApplied) throw createHttpError(400, 'Doanh nghiệp đang được sinh viên đăng ký không thể xóa.');
 
-		// Xóa
-		const businessDelete = await BusinessModel.findByIdAndRemove(id);
+		const deletedBusiness = await BusinessModel.findByIdAndRemove(id);
+
+		if (!deletedBusiness) throw createHttpError(404, 'Doanh nghiệp này không tồn tại!');
 
 		return res.status(200).json(businessDelete);
 	} catch (error) {
-		return res.status(error.statusCode || 500).json({
-			statusCode: error.statusCode || 500,
-			message: error.message || 'Internal Server Error'
-		});
+		const httpException = new HttpException(error);
+		return res.status(httpException.statusCode).json(httpException);
 	}
 };
 
+//! DEPRECATE
 // [POST] /api/business/new
 export const createbusiness = async (req, res) => {
 	const { business_code } = req.body;
@@ -186,13 +173,12 @@ export const createbusiness = async (req, res) => {
 
 		return res.status(201).json(newBusiness);
 	} catch (error) {
-		return res.status(error.statusCode || 500).json({
-			statusCode: error.statusCode || 500,
-			message: error.message || 'Internal Server Error'
-		});
+		const httpException = new HttpException(error);
+		return res.status(httpException.statusCode).json(httpException);
 	}
 };
 
+//! DEPRECATE
 // [PATCH] /api/business/:id
 export const updateBusiness = async (req, res) => {
 	const data = req.body;
@@ -220,16 +206,8 @@ export const updateBusiness = async (req, res) => {
 
 		return res.status(201).json(itemBusinessUpdate);
 	} catch (error) {
-		if (error.code === 11000) {
-			return res.status(409).json({
-				statusCode: 409,
-				message: 'Mã doanh nghiệp đã tồn tại'
-			});
-		}
-		return res.status(error.statusCode || 500).json({
-			statusCode: error.statusCode || 500,
-			message: error.message || 'Internal Server Error'
-		});
+		const httpException = new HttpException(error);
+		return res.status(httpException.statusCode).json(httpException);
 	}
 };
 
@@ -237,36 +215,30 @@ export const updateBusiness = async (req, res) => {
 export const getBusiness = async (req, res) => {
 	try {
 		const id = req.params.id;
-		const result = await BusinessModel.findOne({
-			_id: id
-		});
 
-		if (!result) {
-			throw createHttpError(404, 'Doanh nghiệp cần tìm không tồn tại');
-		}
+		if (!isValidObjectId(id)) throw createHttpError(400, 'ID không hợp lệ');
+
+		const result = await BusinessModel.findById(id);
+
+		if (!result) throw createHttpError(404, 'Doanh nghiệp cần tìm không tồn tại');
 
 		return res.status(200).json(result);
 	} catch (error) {
-		return res.status(error.statusCode || 500).json({
-			statusCode: error.statusCode || 500,
-			message: error.message || 'Internal Server Error'
-		});
+		const httpException = new HttpException(error);
+		return res.status(httpException.statusCode).json(httpException);
 	}
 };
 
+//! DEPRECATE
 // [PATCH] /api/business (Tái sử dụng lại dữ liệu doanh nghiệp từ kỳ cũ)
 export const updateMany = async (req, res) => {
 	try {
 		const data = req.body;
 		const campus = req.campusManager || req.campusStudent;
 
-		if (!Array.isArray(data)) {
-			throw createHttpError(400, 'Data body gửi lên không phải type array');
-		}
+		if (!Array.isArray(data)) throw createHttpError(400, 'Data body gửi lên không phải type array');
 
-		if (data.length === 0) {
-			throw createHttpError(204);
-		}
+		if (!data.length) throw createHttpError(400, 'Sai dữ liệu');
 
 		const semester = await getCurrentSemester(campus);
 
@@ -285,9 +257,7 @@ export const updateMany = async (req, res) => {
 			message: `Đã chuyển doanh nghiệp sang kỳ học ${semester.name}!`
 		});
 	} catch (error) {
-		return res.status(error.statusCode || 500).json({
-			statusCode: error.statusCode || 500,
-			message: error.message || 'Internal Server Error'
-		});
+		const httpException = new HttpException(error);
+		return res.status(httpException.statusCode).json(httpException);
 	}
 };
