bc9c0f0e7b6d883a6e93d292f398d98536b0a0e7
Maladroy <hoamgchilam@pm.me>
2023-06-01 14:25:03 +0700

    hotfix: fix entity payload's too large

--- src/index.js
+++ src/index.js
@@ -7,18 +7,18 @@ import swaggerUI from 'swagger-ui-express';
 import rootRouter from './api/routes';
 import swaggerOptions from './config/swagger.config';
 import connectMongo from './database/mongo.db';
-
+import bodyParser from 'body-parser';
 const app = express();
 
 // Route
 
 // middleware
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
 app.use(morgan('tiny'));
 app.use(express.json({ limit: '50mb' }));
 app.use(compression({ level: 6, threshold: 1024 })); // compress data if payload is too large
-
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 // Swagger
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerOptions));
 
@@ -34,7 +34,7 @@ app.listen(PORT, () => {
 app.get('/', (req, res) =>
 	res.json({
 		message: 'Connected to server !',
-		status: 200,
+		status: 200
 	})
 );
 
