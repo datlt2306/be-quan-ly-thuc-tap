8d933b7b0fad1f6a60abd1ae6b358a3e2d4391ff
Maladroy <hoamgchilam@pm.me>
2023-06-05 14:11:46 +0700

    fix: update numberOfTime, applyIntern message

--- src/api/controllers/reportForm.controller.js
+++ src/api/controllers/reportForm.controller.js
@@ -6,6 +6,7 @@ import studentModel from '../models/student.model';
 import { uploadFile } from '../services/googleDrive.service';
 import { formSchema, reportSchema } from '../validation/reportForm.validation';
 import MailTypes from '../constants/mailTypes';
+import { HttpException } from '../../utils/httpException';
 
 export const report = async (req, res) => {
 	let data, error, result, uploadedFile;
@@ -16,18 +17,18 @@ export const report = async (req, res) => {
 		const filter = { mssv: mssv, email: email, _id };
 		const student = await studentModel.findOne(filter);
 
-		if (!student) throw createHttpError(403, 'Không tìm thấy thông tin sinh viên');
+		if (!student) throw createHttpError(404, 'Không tìm thấy thông tin sinh viên');
 
-		if (!student.internshipTime) throw createHttpError(403, 'Không tìm thấy thời gian thực tập');
+		if (!student.internshipTime) throw createHttpError(400, 'Không tìm thấy thời gian thực tập');
 
-		if (!endInternShipTime) throw createHttpError(403, 'Chưa nhập thời gian kết thúc thực tập');
+		if (!endInternShipTime) throw createHttpError(400, 'Chưa nhập thời gian kết thúc thực tập');
 
 		const startTimeReport = moment(student.internshipTime).valueOf();
 		const endTimeReport = moment(endInternShipTime).valueOf();
 		const checkTimeReport = endTimeReport > startTimeReport;
 		const [file] = req.files;
 
-		if (!checkTimeReport) throw createHttpError(403, 'Thời gian kết thúc thực tập phải lớn hơn thời gian bắt đầu!');
+		if (!checkTimeReport) throw createHttpError(400, 'Thời gian kết thúc thực tập phải lớn hơn thời gian bắt đầu!');
 
 		switch (student.statusCheck) {
 			// Đang thực tập
@@ -46,7 +47,9 @@ export const report = async (req, res) => {
 
 				error = reportSchema.validate(data).error;
 
-				if (error) throw createHttpError(403, 'Sai dữ liệu: ' + error.message);
+				if (error) throw createHttpError(400, 'Sai dữ liệu: ' + error.message);
+
+				data.numberOfTime = 0;
 
 				result = await studentModel.findOneAndUpdate(filter, data, {
 					new: true
@@ -57,9 +60,9 @@ export const report = async (req, res) => {
 
 			// Đã nộp báo cáo
 			case 7:
-				if (!student.report) throw createHttpError(500, 'Không tìm thấy thông tin báo cáo');
+				if (!student.report) throw createHttpError(404, 'Không tìm thấy thông tin báo cáo');
 
-				throw createHttpError(403, 'Thông tin báo cáo đã tồn tại và đang chờ xác nhận!');
+				throw createHttpError(400, 'Thông tin báo cáo đã tồn tại và đang chờ xác nhận!');
 
 			// Sửa báo cáo
 			case 8:
@@ -78,20 +81,23 @@ export const report = async (req, res) => {
 
 				error = reportSchema.validate(data).error;
 
-				if (error) throw createHttpError(403, 'Sai dữ liệu: ' + error.message);
+				if (error) throw createHttpError(400, 'Sai dữ liệu: ' + error.message);
 
-				result = await studentModel.findOneAndUpdate(filter, data, {
-					new: true
-				});
+				result = await studentModel.findOneAndUpdate(
+					filter,
+					{ ...data, numberOfTime: student.numberOfTime + 1 },
+					{
+						new: true
+					}
+				);
 				await sendMail({ recipients: email, ...getMailTemplate(MailTypes.UPDATED_REPORT) });
 				return res.status(200).json({ message: 'Sửa báo cáo thành công', result });
 			default:
-				throw createHttpError(403, 'Bạn không đủ điều kiện nộp báo cáo');
+				throw createHttpError(400, 'Bạn không đủ điều kiện nộp báo cáo');
 		}
 	} catch (error) {
-		return res.status(error.statusCode || 500).json({
-			message: error.message || 'Đã xảy ra lỗi! Vui lòng kiểm tra lại thông tin báo cáo!'
-		});
+		const httpException = new HttpException(error);
+		return res.status(httpException.statusCode).json(httpException);
 	}
 };
 
@@ -104,26 +110,19 @@ export const form = async (req, res) => {
 		const [file] = req.files;
 		const findStudent = await studentModel.findOne(filter);
 
-		if (!findStudent) return res.status(404).json({ status: false, message: 'Đã xảy ra lỗi! Vui lòng đăng ký lại!' });
+		if (!findStudent) throw createHttpError(404, 'Đã xảy ra lỗi! Vui lòng đăng ký lại!');
 
 		switch (findStudent.statusCheck) {
 			// Chờ kiểm tra CV
 			case 0:
-				if (!findStudent.CV)
-					return res.status(500).json({
-						message: 'CV không tồn tại trên hệ thống'
-					});
-
-				return res.status(403).json({
-					message: 'CV phải được duyệt trước khi nộp biên bản!'
-				});
+				if (!findStudent.CV) throw createHttpError(404, 'CV không tồn tại trên hệ thống');
+				throw createHttpError(400, 'CV phải được duyệt trước khi nộp biên bản!');
 
 			// Nhận CV
 			case 2:
-				if (!findStudent.support == 1)
-					return res.status(403).json({
-						message: 'Bạn chưa gửi form nhờ nhà trường hỗ trợ'
-					});
+				if (!findStudent.support == 1) throw createHttpError(400, 'Bạn chưa gửi form nhờ nhà trường hỗ trợ');
+				if (findStudent.numberOfTime >= 3)
+					throw createHttpError(400, 'Tài khoạn của bạn đã vượt quá số lần đăng ký');
 
 				uploadedFile = await uploadFile(file);
 
@@ -135,7 +134,7 @@ export const form = async (req, res) => {
 				};
 
 				error = formSchema.validate(data).error;
-				if (error) throw createHttpError(403, 'Sai dữ liệu: ' + error.message);
+				if (error) throw createHttpError(400, 'Sai dữ liệu: ' + error.message);
 
 				result = await studentModel.findOneAndUpdate(filter, data, {
 					new: true
@@ -146,15 +145,11 @@ export const form = async (req, res) => {
 			// Không đủ điều kiện
 			case 3:
 			case 10:
-				return res.status(403).json({
-					message: 'Bạn không đủ điều kiện nộp biên bản!'
-				});
+				throw createHttpError(400, 'Bạn không đủ điều kiện nộp biên bản!');
 
 			// Đã nộp biên bản
 			case 4:
-				return res.status(403).json({
-					message: 'Bạn đã nộp biên bản!'
-				});
+				throw createHttpError(400, 'Bạn đã nộp biên bản!');
 
 			// Sửa biên bản
 			case 5:
@@ -170,11 +165,11 @@ export const form = async (req, res) => {
 
 				error = formSchema.validate(data).error;
 
-				if (error) throw createHttpError(403, 'Sai dữ liệu: ' + error.message);
+				if (error) throw createHttpError(400, 'Sai dữ liệu: ' + error.message);
 
 				result = await studentModel.findOneAndUpdate(
 					filter,
-					{ ...data, note: null },
+					{ ...data, note: null, numberOfTime: findStudent.numberOfTime + 1 },
 					{
 						new: true
 					}
@@ -184,7 +179,7 @@ export const form = async (req, res) => {
 			// Đã đăng ký
 			case 11:
 				if (!findStudent.support == 0)
-					throw createHttpError(403, 'Form tự tìm của bạn chưa được duyệt hoặc server bị lỗi!');
+					throw createHttpError(400, 'Form tự tìm của bạn chưa được duyệt hoặc server bị lỗi!');
 
 				uploadedFile = await uploadFile(file);
 
@@ -197,7 +192,9 @@ export const form = async (req, res) => {
 
 				error = formSchema.validate(data).error;
 
-				if (error) throw createHttpError(403, 'Sai dữ liệu: ' + error.message);
+				if (error) throw createHttpError(400, 'Sai dữ liệu: ' + error.message);
+
+				data.numberOfTime = 0;
 
 				result = await studentModel.findOneAndUpdate(filter, data, {
 					new: true
@@ -209,8 +206,7 @@ export const form = async (req, res) => {
 				throw new Error();
 		}
 	} catch (error) {
-		return res.status(error.statusCode || 500).json({
-			message: error.message || 'Có lỗi xảy ra! Vui lòng quay lại sau ít phút'
-		});
+		const httpException = new HttpException(error);
+		return res.status(httpException.statusCode).json(httpException);
 	}
 };
--- src/api/controllers/requestStudent.controller.js
+++ src/api/controllers/requestStudent.controller.js
@@ -1,102 +1,103 @@
-import requestStudentToManagerModel from "../models/requestStudentToManager.model";
-import Student from "../models/student.model";
-import {
-  defaultCvStudent,
-  defaultForm,
-  defaultReport,
-} from "../../utils/defaultValueStudent";
+import requestStudentToManagerModel from '../models/requestStudentToManager.model';
+import Student from '../models/student.model';
+import { defaultCvStudent, defaultForm, defaultReport } from '../../utils/defaultValueStudent';
+import studentModel from '../models/student.model';
 export function getListRequestStudent(req, res) {}
 
 export async function sendRequestToManager(req, res) {
-  const findRequest = await requestStudentToManagerModel.findOne({
-    userId: req.body.userId,
-  });
+	const findRequest = await requestStudentToManagerModel.findOne({
+		userId: req.body.userId
+	});
 
-  try {
-    if (findRequest && findRequest.type === req.body.type) {
-      return res.status(400).json({
-        success: false,
-        message: "Yêu cầu đã tồn tại",
-      });
-    } else {
-      await requestStudentToManagerModel.create(req.body);
-      res.status(201).json({
-        success: true,
-        message: "Thành công",
-      });
-    }
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-      error,
-      message: "Có lỗi xảy ra",
-    });
-  }
+	try {
+		if (findRequest && findRequest.type === req.body.type) {
+			return res.status(400).json({
+				success: false,
+				message: 'Yêu cầu đã tồn tại'
+			});
+		} else {
+			await requestStudentToManagerModel.create(req.body);
+			res.status(201).json({
+				success: true,
+				message: 'Thành công'
+			});
+		}
+	} catch (error) {
+		res.status(400).json({
+			success: false,
+			error,
+			message: 'Có lỗi xảy ra'
+		});
+	}
 }
 
 export async function getRequestOfStudent(req, res) {
-  try {
-    const data = await requestStudentToManagerModel
-      .find(req.query)
-      .populate("userId");
-    res.status(200).json({
-      success: true,
-      data: data,
-    });
-  } catch (err) {
-    res.status(401).json({
-      success: false,
-      err,
-    });
-  }
+	try {
+		const data = await requestStudentToManagerModel.find(req.query).populate('userId');
+		res.status(200).json({
+			success: true,
+			data: data
+		});
+	} catch (err) {
+		res.status(401).json({
+			success: false,
+			err
+		});
+	}
 }
 
 export async function resetStudent(req, res) {
-  const { type, id, useId } = req.body;
-  let valueReset = {};
-  if (type === process.env.cv) {
-    valueReset = defaultCvStudent;
-  }
-  if (type === process.env.form) {
-    valueReset = defaultForm;
-  }
-  if (type === process.env.report) {
-    valueReset = defaultReport;
-  }
+	const { type, id, useId } = req.body;
+	let valueReset = {};
+	if (type === process.env.cv) {
+		valueReset = defaultCvStudent;
+	}
+	if (type === process.env.form) {
+		valueReset = defaultForm;
+	}
+	if (type === process.env.report) {
+		valueReset = defaultReport;
+	}
 
-  try {
-    Student.findOneAndUpdate({ _id: req.params.id }, valueReset, { new: true })
-      .then((r) => requestStudentToManagerModel.findByIdAndUpdate(id, {
-        status: 2
-      }, { new: true }))
-      .then((r) =>
-        res.status(200).json({
-          success: true,
-          message: "Thành công",
-        })
-      );
-  } catch (error) {
-    res.status({
-      success: false,
-      message: "Có lỗi sảy ra",
-    });
-  }
+	try {
+		Student.findOneAndUpdate({ _id: req.params.id }, valueReset, { new: true })
+			.then((r) =>
+				requestStudentToManagerModel.findByIdAndUpdate(
+					id,
+					{
+						status: 2
+					},
+					{ new: true }
+				)
+			)
+			.then((r) =>
+				res.status(200).json({
+					success: true,
+					message: 'Thành công'
+				})
+			);
+	} catch (error) {
+		res.status({
+			success: false,
+			message: 'Có lỗi sảy ra'
+		});
+	}
 }
 
 export async function cancelResetStudent(req, res) {
-  try {
-    await requestStudentToManagerModel.findByIdAndUpdate(req.params.id,{
-      status: 3
-    });
+	try {
+		await requestStudentToManagerModel.findByIdAndUpdate(req.params.id, {
+			status: 3
+		});
 
-    res.status(200).json({
-      success: true,
-      message: "Thành công",
-    });
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-      message: "Thất bại",
-    });
-  }
+		res.status(200).json({
+			success: true,
+			message: 'Thành công'
+		});
+	} catch (error) {
+		res.status(400).json({
+			success: false,
+			message: 'Thất bại'
+		});
+	}
 }
