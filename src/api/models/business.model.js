d32c94594aca35d0323a857b5c4835f75f60a934
quanghiep03198 <quanghiep03198@gmail.com>
2023-06-07 03:24:56 +0700

    Fix business pagination

--- src/api/models/business.model.js
+++ src/api/models/business.model.js
@@ -1,64 +1,70 @@
-const mongoose = require('mongoose');
-const mongoosePaginate = require('mongoose-paginate-v2');
+import mongoose from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2';
+import mongooseAutoPopulate from 'mongoose-autopopulate';
 
-const { ObjectId } = mongoose.Schema;
-const businessSchema = mongoose.Schema({
-	name: {
-		type: String,
-		required: true
+const businessSchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true
+		},
+		internship_position: {
+			type: String,
+			required: true
+		},
+		major: {
+			type: mongoose.Types.ObjectId,
+			ref: 'Major',
+			required: true
+		},
+		amount: {
+			type: Number,
+			default: 0
+		},
+		address: {
+			type: String
+		},
+		semester_id: {
+			type: mongoose.Types.ObjectId,
+			ref: 'Semester',
+			autopopulate: true
+		},
+		campus_id: {
+			type: mongoose.Types.ObjectId,
+			ref: 'Campus',
+			autopopulate: true
+		},
+		tax_code: {
+			type: String,
+			required: true
+		},
+		business_code: {
+			type: String,
+			required: true
+		},
+		requirement: {
+			type: String
+		},
+		description: {
+			type: String
+		},
+		benefit: {
+			type: String
+		},
+		status: {
+			type: Number,
+			default: 1
+		}
 	},
-	internship_position: {
-		type: String,
-		required: true
-	},
-	major: {
-		type: ObjectId,
-		ref: 'Major',
-		required: true
-	},
-	amount: {
-		type: Number,
-		default: 0
-	},
-	address: {
-		type: String
-	},
-	semester_id: {
-		type: ObjectId,
-		ref: 'Semester'
-	},
-	campus_id: {
-		type: ObjectId,
-		ref: 'Campus'
-	},
-	tax_code: {
-		type: String,
-		required: true
-	},
-	business_code: {
-		type: String,
-		required: true
-	},
-	requirement: {
-		type: String
-	},
-	description: {
-		type: String
-	},
-	benefit: {
-		type: String
-	},
-	status: {
-		type: Number,
-		default: 1
-	},
-	created_at: {
-		type: Date,
-		default: Date.now
+	{
+		timestamps: true,
+		versionKey: false,
+		toJSON: { virtuals: true }
 	}
-});
+);
 
 businessSchema.index({ tax_code: 1, business_code: 1, campus_id: 1 }, { unique: true });
 businessSchema.plugin(mongoosePaginate);
+businessSchema.plugin(mongooseAutoPopulate);
 
-module.exports = mongoose.model('Business', businessSchema);
+export default mongoose.model('Business', businessSchema);
