7c57ab9aaa44d74df5bc7cb81209b4fdb7d179bf
quanghiep03198 <quanghiep03198@gmail.com>
2023-06-14 18:26:11 +0700

    Update webpack config

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	mode: 'production',
+	target: 'node',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'swc-loader',
+					options: {
+						jsc: {
+							parser: {
+								syntax: 'ecmascript'
+							}
+						}
+					}
+				}
+			}
+		]
+	},
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new TerserPlugin({
+				terserOptions: {
+					format: {
+						comments: false
+					}
+				},
+				extractComments: false
+			})
+		]
+	},
+	externals: [nodeExternals()]
+};
