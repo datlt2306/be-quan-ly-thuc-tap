6e12ea77e4be4601ea7d1d64bc593368b89c819f
Quang Hiep <quanghiep03198@gmail.com>
2023-12-15 18:31:09 +0700

    Update

--- src/config/nodemailer.config.js
+++ src/config/nodemailer.config.js
@@ -1,18 +1,9 @@
 import nodemailer from 'nodemailer';
 import 'dotenv/config';
 
-// const transporter = nodemailer.createTransport({
-// 	service: 'gmail',
-// 	port: 465,
-// 	auth: {
-// 		user: process.env.USER_EMAIL,
-// 		pass: process.env.PASS_EMAIL
-// 	},
-// 	tls: {
-// 		rejectUnauthorized: false
-// 	}
-// });
-
+/**
+ * @deprecated
+ */
 const transporter = nodemailer.createTransport({
 	host: 'smtp-mail.outlook.com',
 	secureConnection: false,
--- src/database/mongo.db.js
+++ src/database/mongo.db.js
@@ -5,7 +5,7 @@ const connectMongo = async () => {
 	try {
 		// database
 		mongoose.set('strictQuery', false);
-		await mongoose.connect(process.env.DATABASE, { connectTimeoutMS: 5000, maxPoolSize: 100 });
+		await mongoose.connect(process.env.DATABASE, { maxPoolSize: 100 });
 		console.log('\x1b[1;42m Info \x1b[0m', 'MongoDB:', 'Connected to database');
 	} catch (error) {
 		console.log('\x1b[1;41m Error \x1b[0m', 'Failed to connect to Mongo');
